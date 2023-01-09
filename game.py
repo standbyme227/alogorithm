a45793d12e7040243b6750374d4e551d4ee941c5
mini <mini@butfitseoul.com>
2023-01-09 16:56:57 +0900

    Game c2
    전체가 1인 경우

--- game.py
+++ game.py
@@ -1,8 +1,32 @@
 # 11
 # maps = [[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 1], [0, 0, 0, 0, 1]]
+
+
 # maps = [[1, 1, 1, 1, 1], [1, 0, 0, 0, 1], [1, 0, 0, 0, 1], [1, 0, 0, 0, 0], [1, 1, 1, 1, 1]]
-# maps = [[1, 1, 1, 1, 1], [0, 0, 0, 0, 1], [1, 1, 1, 1, 1], [1, 0, 0, 0, 0], [1, 1, 1, 1, 1]]
-maps = [[1, 1, 1, 0, 0], [0, 0, 1, 1, 1]]
+
+# maps = [
+#     [1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1],
+#
+# ]
+maps = [
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 0, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+]
+# maps = [[1, 1, 1, 0, 0], [0, 0, 1, 1, 1]]
 
 
 # maps = [[1, 1, 1], [1, 1, 1]]
@@ -43,19 +67,38 @@ def check_blocks(maps, x, y, n, m):
 def solution(maps):
     n = len(maps[0])
     m = len(maps)
+    shortcut = n + m - 1
 
     if n > 1 and m > 1:
         if 0 == maps[0][1] == maps[1][0] or 0 == maps[m - 1][n - 2] == maps[m - 2][n - 1]:
             return -1
 
     map_dict = dict()
+    zero = 0
+    other = 0
+
     for y, i in enumerate(maps):
         for x, j in enumerate(i):
+            k = maps[y][x]
+            if k == 1:
+                other += 1
+            else:
+                zero += 1
+
             map_dict[(x, y)] = []
             results = check_blocks(maps, x, y, n, m)
             for result in results:
                 if result != (x, y) and result not in map_dict[(x, y)]:
                     map_dict[(x, y)].append(result)
+
+    if zero > (n * m) * (2 / 3):
+        return -1
+
+    elif zero == 0:
+        return shortcut
+
+    elif zero < n and zero < m:
+        return shortcut
 
     starts = [
         [[], (n - 1, m - 1), 0]
@@ -67,36 +110,31 @@ def solution(maps):
         temp = []
         for i in starts:
             # 시작 지점
-            start = i[1]
-            prev_list = i[0]
+            target = i[1]
             cnt = i[2]
             cnt += 1
 
-            if start == (0, 0):
-                if answer == 0:
+            if target == (0, 0):
+                if answer == 0 or answer > cnt:
                     answer = cnt
-                else:
-                    if answer > cnt:
-                        answer = cnt
+
+                if shortcut == answer:
+                    return answer
                 continue
 
             if answer != 0 and cnt >= answer:
                 continue
 
-            x = start[0]
-            y = start[1]
+            x = target[0]
+            y = target[1]
 
-            next_list = []
-            prev_list.append(start)
+            i[0].append(target)
 
             if maps[y][x] != 0:
                 results = map_dict[(x, y)]
                 for result in results:
-                    if result not in prev_list:
-                        next_list.append(result)
-
-            for nxt in next_list:
-                temp.append([prev_list, nxt, cnt])
+                    if result not in i[0]:
+                        temp.append([i[0], result, cnt])
         starts = temp
     # print(answer if answer else -1)
     return answer if answer else -1
